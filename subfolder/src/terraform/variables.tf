ab181b659b031a5006277fbb4cda953046b6f6c7
andriizavoiko <167066629+andriizavoiko@users.noreply.github.com>
2024-05-21 19:40:58 +0300

    Create variables.tf

---
+++ subfolder/src/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "internal_port" {
+  description = "Internal port"
+  type        = number
+  default     = 80
+}
+
+variable "external_port" {
+  description = "External port"
+  type        = number
+  default     = 8080
+}
